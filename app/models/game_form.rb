8e3a83bea8053eed86dc7ceff3570a3c9a8da766
IanGrubb <yangrubb@gmail.com>
2019-07-30 01:04:18 -0400

    creating game_form creates game_questions

--- app/models/game_form.rb
+++ app/models/game_form.rb
@@ -2,9 +2,27 @@ class GameForm < ApplicationRecord
     has_many :games
     has_many :users, through: :games
     has_many :game_questions
+
+    after_create :make_game_questions
     
     def questions
         self.game_questions.collect{|game_question| game_question.question}
+    end
+
+    def make_game_questions
+
+        game_form = GameForm.last
+
+        style_hash = {syn: {class: QuestionSyn, question_number: 5, question_type: "QuestionSyn"}}
+
+
+        style_hash[game_form.style.to_sym][:class].all.sample(style_hash[game_form.style.to_sym][:question_number]).each do |question|
+
+
+            GameQuestion.create(game_form_id: game_form.id, question_type: style_hash[game_form.style.to_sym][:question_type], question_id: question.id)
+
+        end
+        
     end
 
 
