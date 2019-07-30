8751be5c7c36f47c542d920c1f0aeca9edcd20ff
IanGrubb <yangrubb@gmail.com>
2019-07-30 15:36:50 -0400

    admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Ian", password_digest: "ianpw", admin?: true)
+User.create(username: "MildlyConfused", password_digest: "benpw" , admin?: true)
