93e679ea6e3149c0a66d3b7137f24f01f4cece8a
Ben Braunstein <benzbraunstein@gmail.com>
2019-07-30 17:05:22 -0400

    modified controller

--- app/controllers/question/mats_controller.rb
+++ app/controllers/question/mats_controller.rb
@@ -1,19 +1,16 @@
 
 class Question::MatsController < ApplicationController
-    def index
-      @questions = QuestionMat.all
-    end
-  
-    def show
-      @question = QuestionMat.find(params[:id])
-    end
-  
-    def new
-  
-    end
-  
-    def destroy
-  
-    end
-    
-  end 
+  def index
+    @questions = QuestionMat.all
+  end
+
+  def show
+    @question = QuestionMat.find(params[:id])
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
