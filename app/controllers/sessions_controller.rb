52e31492cffbf8b689e1eb934f0bc719ebd8baed
IanGrubb <yangrubb@gmail.com>
2019-08-01 14:59:33 -0400

    more css

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[message]
+      flash[message] = "Sorry, you don't have a matching username and password."
     end
   end
 
